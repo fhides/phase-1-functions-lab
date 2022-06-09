9ce2e7a6a20e6d652ac7968de862dd8fc461ca2a
fhidesjoseph <fhidesjoseph@gmail.com>
2022-06-09 22:45:53 +0300

    commit tree 7d36678b6016deb6af58565efad69ba375bed41d

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const distanceFromHqInBlocks = function (sum){
+
+    return sum> 42 ?  sum - 42 : 42 - sum;
+}
+
+const distanceFromHqInFeet = function (sum){
+    return sum > 42 ?  (sum - 42)*264 : (42 - sum)*264;
+}
+const distanceTravelledInFeet =function(start , destination) {
+    return destination > start? (destination- start) * 264 : (start - destination) * 264 
+  
+}
+const calculatesFarePrice = function (start, destination) {
+    
+    return distanceTravelledInFeet(start, destination) < 400 ? 0
+    : distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet
+    (start, destination) <= 2000 ?  (distanceTravelledInFeet
+        (start, destination)-400) * 0.02
+    : distanceTravelledInFeet(start, destination) >2000 && distanceTravelledInFeet
+    (start, destination) <= 2500 ? 25: 'cannot travel that far';
+   
+
+}
